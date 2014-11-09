ccbbff3d8b1ce222d1d92e2d073729f37d10c562
Anderson Danilo <contato@andersondanilo.com>
2014-11-09 11:12:17 -0200

    segundo commit

--- ServiceProvider.php
+++ ServiceProvider.php
@@ -1,27 +1,11 @@
 <?php
 
-namespace Tee\Portfolio;
-
-use Tee\Portfolio\Widgets\PortfolioBoxList;
-use Tee\System\Widget;
-use Event;
+namespace Tee\Backup;
 
 class ServiceProvider extends \Illuminate\Support\ServiceProvider {
 
     public function register()
     {
-        // registra os widgets
-        Widget::register(
-            'portfolioBoxList',
-            __NAMESPACE__.'\\Widgets\\PortfolioBoxList'
-        );
-
-        Event::listen('admin::menu.load', function($menu) {
-            $format = '<img src="%s" class="fa" />&nbsp;&nbsp;<span>%s</span>';
-            $menu->add(
-                sprintf($format, moduleAsset('portfolio', 'images/icon_portfolio.png'), 'Portfólio'),
-                route('admin.portfolio.index')
-            );
-        });
+        
     }
 }
